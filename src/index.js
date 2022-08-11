72ccf042b9b3a540c68d3651262b021b411022c1
Christian <chrisburg1222@gmail.com>
2022-08-11 12:17:04 -0500

    M

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas.js';
+// import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas.js';
 
 // promesaLenta( console.log );
 // promesaMedia( console.log );
 // promesaRapida( console.log);
  
 
-Promise.race( [ promesaLenta, promesaMedia, promesaRapida])
-             .then( console.log )
-             .catch( console.warn );  
+// Promise.race( [ promesaLenta, promesaMedia, promesaRapida])
+//              .then( console.log )
+//              .catch( console.warn );  
 
              
